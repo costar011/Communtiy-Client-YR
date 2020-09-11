201c3564372891efec12c413deab00bedc38ae18
YERIM <dnaer456@gmail.com>
2020-09-11 14:53:44 +0900

    create MH0 screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Hedaer from "./screens/Header";
+import MH0 from "./screens/MH0";
 
 class App extends React.Component {
   render() {
-    return <div className="App">App</div>;
+    return (
+      <div className="App">
+        <Route path="/" component={Hedaer} />
+        <Route exact path="/" component={MH0} />
+      </div>
+    );
   }
 }
 
---
+++ src/components/screens/MH0.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Reveal from "react-reveal/Reveal";
+import Fade from "react-reveal/Fade";
+import { Link } from "react-router-dom";
+
+const menus = [];
+
+class MH0 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menus: [
+        {
+          menusList: "List Board",
+          link: "/listboard",
+        },
+
+        {
+          menusList: "Free BOARD",
+          link: "/freeboard",
+        },
+      ],
+    };
+  }
+  render() {
+    const { menus } = this.state;
+    return (
+      <main className="MH0">
+        <Reveal>
+          <h1 className="MH0__title">YR Board</h1>
+        </Reveal>
+        <div className="MH0__menuBox">
+          {menus.map((menu, idx) => {
+            return (
+              <Fade bottom delay={idx * 300} key={idx}>
+                <Link to={menu.link}>
+                  <div className="MH0__menuBox__list">{menu.menusList}</div>
+                </Link>
+              </Fade>
+            );
+          })}
+        </div>
+      </main>
+    );
+  }
+}
+
+export default MH0;
